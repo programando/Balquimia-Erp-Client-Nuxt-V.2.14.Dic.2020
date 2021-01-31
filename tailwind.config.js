36fd3f2614751da9db6a21c032eb1a05cf37dbf1
programando <jhonjamesmg@hotmail.com>
2021-01-30 20:07:40 -0500

    Configurar colores box messages

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,9 +13,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary  : '#D3500A',
-        secondary: '#ce550a',
-        extra    : '#0f3057'
+        primary             : '#D3500A',
+        secondary           : '#ce550a',
+        extra               : '#0f3057',
+        msgErrorBackground  : '#0f3057',
+        msgErrorBody        : '#0f3057',
+        msgInfoBackground   : '#0f3057',
+        msgInfoBody         : '#0f3057',
+        msgSuccessBackground: '#0f3057',
+        msgSuccessBody      : '#0f3057',
+        msgWarningBackground: '#0f3057',
+        msgWarningBody      : '#0f3057',
       },
 
       fontFamily: {
