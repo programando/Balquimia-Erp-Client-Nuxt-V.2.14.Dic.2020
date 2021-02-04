9620dd6aeb77f23d68c322ffb0a61bcb848d8633
Luis Morales <luisjose.morales2@gmail.com>
2021-02-03 21:08:55 -0400

    agregando elemento al card

--- pages/computron/dashboard/index.vue
+++ pages/computron/dashboard/index.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="font-latos">
-     
     <div class="min-h-screen px-4 pb-40 bg-gray-100 pt-36">
       <div class="mx-2">
         <p class="mx-10 text-lg font-bold lg:text-xl">Comercial</p>
-        <div class="grid grid-cols-1 gap-8 mx-6 mt-6 lg:gap-4 lg:mx-0 xl:mx-8 xl:gap-10 sm:grid-cols-2 lg:grid-cols-4">
-         
+        <div
+          class="grid grid-cols-1 gap-8 mx-6 mt-6 lg:gap-4 lg:mx-0 xl:mx-8 xl:gap-10 sm:grid-cols-2 lg:grid-cols-5"
+        >
+          <div
+            class="p-4 mx-4 transition-shadow bg-white border rounded-lg shadow-sm cursor-pointer hover:shadow-lg"
+          >
+            <div class="flex items-center justify-center space-x-4">
+              <div class="rounded-md">
+                <img class="h-12" src="/images/dashboard/cartera.svg" alt="" />
+              </div>
+              <div class="">
+                <span class="text-gray-900">Cartera</span>
+              </div>
+            </div>
+          </div>
+
           <nuxt-link to="/computron/comercial/pedidos-registro">
             <div
               class="p-4 mx-4 transition-shadow bg-white border rounded-lg shadow-sm cursor-pointer hover:shadow-lg"
@@ -19,23 +32,18 @@
                   />
                 </div>
                 <div class="">
-                  <span class="text-gray-900 ">Registro de Pedidos</span>
+                  <span class="text-gray-900">Registro de Pedidos</span>
                 </div>
               </div>
             </div>
           </nuxt-link>
-
 
           <div
             class="p-4 mx-4 transition-shadow bg-white border rounded-lg shadow-sm cursor-pointer hover:shadow-lg"
           >
             <div class="flex items-center justify-center space-x-4">
               <div class="rounded-md">
-                <img
-                  class="h-12"
-                  src="/images/dashboard/sales.svg"
-                  alt=""
-                />
+                <img class="h-12" src="/images/dashboard/sales.svg" alt="" />
               </div>
               <div class="">
                 <span class="text-gray-900">Ventas por Cliente</span>
@@ -47,11 +55,7 @@
           >
             <div class="flex items-center justify-center space-x-4">
               <div class="rounded-md">
-                <img
-                  class="h-12"
-                  src="/images/dashboard/list.svg"
-                  alt=""
-                />
+                <img class="h-12" src="/images/dashboard/lista-precios.svg" alt="" />
               </div>
               <div class="">
                 <span class="text-gray-900">Lista de Precios</span>
@@ -63,11 +67,7 @@
           >
             <div class="flex items-center justify-center space-x-4">
               <div class="rounded-md">
-                <img
-                  class="h-12"
-                  src="/images/dashboard/pass.svg"
-                  alt=""
-                />
+                <img class="h-12" src="/images/dashboard/autorizacion-cartera.svg" alt="" />
               </div>
               <div class="">
                 <span class="text-gray-900">Autorizacíon Cartera</span>
@@ -77,24 +77,15 @@
         </div>
       </div>
     </div>
-
-
-     
   </div>
 </template>
 <script>
- 
 export default {
-  layout:"layoutComputron",
+  layout: "layoutComputron",
   name: "index",
- 
-  data() {
-    return {
- 
-      
-    };
-  },
 
-  
+  data() {
+    return {};
+  },
 };
 </script>
