2824a6c6429afeab643904723845fc32d72d8218
Luis Jose <luisjose.morales2@gmail.com>
2021-04-02 17:44:41 -0400

    agregando transicion al modal

--- components/library/inputs/InputSearch.vue
+++ components/library/inputs/InputSearch.vue
@@ -1,5 +1,5 @@
 <template>
-  <div >
+  <div>
     <label class="block"> {{ textLabel }}</label>
     <div @click="modal = !modal" class="relative z-10 inline cursor-pointer">
       <img
@@ -17,80 +17,97 @@
     />
 
     <!-- Modal -->
-    <div
-      v-if="modal"
-      class="absolute inset-0 z-20 flex items-center justify-center bg-black bg-opacity-50 max-h-lg "
+    <transition
+      enter-active-class="duration-300 ease-out"
+      enter-class="translate-y-4 opacity-0 sm:translate-y-0 sm:scale-95"
+      enter-to-class="translate-y-0 opacity-100 sm:scale-10"
+      leave-active-class="duration-200 ease-in"
+      leave-class="translate-y-0 opacity-100 sm:scale-100"
+      leave-to-class="translate-y-4 opacity-0 sm:translate-y-0 sm:scale-95"
     >
-      <div class="w-4/6 mt-10 bg-white rounded-lg">
-        <div class="flex items-center justify-between px-4 bg-primary">
-          <h2 class="py-2 text-lg font-semibold text-white ">
-            <strong><slot name='ModalTitle'>Búsqueda</slot></strong>
-          </h2>
-          <div @click="modal = false" class="cursor-pointer ">
-            <img
-              class="h-6"
-              src="/images/dashboard/close-repo.svg"
-              alt="close"
-            />
-          </div>
-        </div>
+      <div
+        v-if="modal"
+        class="absolute inset-0 z-20 flex items-center justify-center bg-black bg-opacity-50 max-h-lg "
+      >
+        <transition
+          enter-active-class="duration-300 ease-out"
+          enter-class="translate-y-4 opacity-0 sm:translate-y-0 sm:scale-95"
+          enter-to-class="translate-y-0 opacity-100 sm:scale-10"
+          leave-active-class="duration-200 ease-in"
+          leave-class="translate-y-0 opacity-100 sm:scale-100"
+          leave-to-class="translate-y-4 opacity-0 sm:translate-y-0 sm:scale-95"
+        >
+          <div class="w-4/6 mt-10 bg-white rounded-lg">
+            <div class="flex items-center justify-between px-4 bg-primary">
+              <h2 class="py-2 text-lg font-semibold text-white ">
+                <strong><slot name="ModalTitle">Búsqueda</slot></strong>
+              </h2>
+              <div @click="modal = false" class="cursor-pointer ">
+                <img
+                  class="h-6"
+                  src="/images/dashboard/close-repo.svg"
+                  alt="close"
+                />
+              </div>
+            </div>
 
-        <div class="px-4 py-2">
-          <InputLabel
-             @change     ="BuscarTercero()"
-             @input      ="BuscarTercero()"
-            :textLabel= "labelTitle"
-            placeholder="Digite para iniciar la búsqueda..."
-            width="w-5/6"
-            v-model="form.filtroBusqueda"
-          ></InputLabel>
-        </div>
+            <div class="px-4 py-2">
+              <InputLabel
+                @change="BuscarTercero()"
+                @input="BuscarTercero()"
+                :textLabel="labelTitle"
+                placeholder="Digite para iniciar la búsqueda..."
+                width="w-5/6"
+                v-model="form.filtroBusqueda"
+              ></InputLabel>
+            </div>
 
-        <div class="h-90">
-          <div class="w-full col-span-2 px-4 py-1 border-2">
-             <table  >
-                    <thead>
-                      <tr>
-                          <th> Nit</th>
-                          <th> Nombre/Razón Soc.</th>
-                          <th> Sucursal</th>
-                          <th></th>
-                      </tr>
-                    </thead>
-                <tbody class="flex-col w-full overflow-y-scroll"
-                  style="height: 40vh"
-                >
-                  <tr v-for="Tercero in tercerosEncontrados" :key="Tercero.id_terc"> 
-                    <td> {{ Tercero.nro_identif}} </td>
-                    <td> {{ Tercero.nom_full}} </td>
-                    <td> {{ Tercero.nom_suc}}</td>
-                      <td class="flex items-center justify-center border-t-2 border-b-2 border-r-2 ">
+            <div class="h-90">
+              <div class="w-full col-span-2 px-4 py-1 border-2">
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Nit</th>
+                      <th>Nombre/Razón Soc.</th>
+                      <th>Sucursal</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody
+                    class="flex-col w-full overflow-y-scroll"
+                    style="height: 40vh"
+                  >
+                    <tr
+                      v-for="Tercero in tercerosEncontrados"
+                      :key="Tercero.id_terc"
+                    >
+                      <td>{{ Tercero.nro_identif }}</td>
+                      <td>{{ Tercero.nom_full }}</td>
+                      <td>{{ Tercero.nom_suc }}</td>
+                      <td
+                        class="flex items-center justify-center border-t-2 border-b-2 border-r-2 "
+                      >
                         <ButtonIcon
                           urlIcon="/images/dashboard/left-arrow-repo.svg"
                           variant="success"
                         ></ButtonIcon>
                       </td>
-                  </tr>
-                </tbody>
-          </table>
-
-
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
-        </div>
-
-       
-
-
+        </transition>
       </div>
-    </div>
-    
+    </transition>
   </div>
 </template>
 
 <script>
 import InputLabel from "@/components/library/inputs/InputLabel";
 import ButtonIcon from "@/components/library/buttons/buttonIcon";
-import Terceros  from "@/models/Terceros";
+import Terceros from "@/models/Terceros";
 
 export default {
   name: "InputSearch",
@@ -108,32 +125,31 @@ export default {
   data() {
     return {
       modal: false,
-      textLabel:'',
-      form :{
-          filtroBusqueda: '',
-          idTercVendedor:''
+      textLabel: "",
+      form: {
+        filtroBusqueda: "",
+        idTercVendedor: ""
       },
-       tercerosEncontrados :[
-         {id: 1, nro_identif: 10, nom_full: 'Alfonzo Perez', nom_suc:'2'},
-         {id: 1, nro_identif: 10, nom_full: 'Alfonzo Perez', nom_suc:'2'},
-         {id: 1, nro_identif: 10, nom_full: 'Alfonzo Perez', nom_suc:'2'},
-         {id: 1, nro_identif: 10, nom_full: 'Alfonzo Perez', nom_suc:'2'},
-         {id: 1, nro_identif: 10, nom_full: 'Alfonzo Perez', nom_suc:'2'},
-         {id: 1, nro_identif: 10, nom_full: 'Alfonzo Perez', nom_suc:'2'},
-         {id: 1, nro_identif: 10, nom_full: 'Alfonzo Perez', nom_suc:'2'},
-         {id: 1, nro_identif: 10, nom_full: 'Alfonzo Perez', nom_suc:'2'},
-         {id: 1, nro_identif: 10, nom_full: 'Alfonzo Perez', nom_suc:'2'}, 
-       ],
+      tercerosEncontrados: [
+        { id: 1, nro_identif: 10, nom_full: "Alfonzo Perez", nom_suc: "2" },
+        { id: 1, nro_identif: 10, nom_full: "Alfonzo Perez", nom_suc: "2" },
+        { id: 1, nro_identif: 10, nom_full: "Alfonzo Perez", nom_suc: "2" },
+        { id: 1, nro_identif: 10, nom_full: "Alfonzo Perez", nom_suc: "2" },
+        { id: 1, nro_identif: 10, nom_full: "Alfonzo Perez", nom_suc: "2" },
+        { id: 1, nro_identif: 10, nom_full: "Alfonzo Perez", nom_suc: "2" },
+        { id: 1, nro_identif: 10, nom_full: "Alfonzo Perez", nom_suc: "2" },
+        { id: 1, nro_identif: 10, nom_full: "Alfonzo Perez", nom_suc: "2" },
+        { id: 1, nro_identif: 10, nom_full: "Alfonzo Perez", nom_suc: "2" }
+      ]
     };
   },
 
   methods: {
     BuscarTercero() {
       this.form.idTercVendedor = this.$store.state.User.IdTercLogueado;
-      Terceros.clientesBuscar ( this.form)
-      .then ( response => {
-          this.tercerosEncontrados= response.data;
-      })
+      Terceros.clientesBuscar(this.form).then(response => {
+        this.tercerosEncontrados = response.data;
+      });
     }
   }
 };
