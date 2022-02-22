96195e84e42fce5d2caed155a9f807eb33fb9f91
Luis Morales <luisjose.morales2@gmail.com>
2022-02-22 13:46:18 -0400

    modificando imagenes slider

--- components/home/slider/Slider.vue
+++ components/home/slider/Slider.vue
@@ -1,20 +1,24 @@
 <template>
-  <div v-swiper="swiperOption" class="relative w-full ml-auto" :loadtheme="false">
+  <div
+    v-swiper="swiperOption"
+    class="relative w-full ml-auto"
+    :loadtheme="false"
+  >
     <div class="swiper-wrapper">
       <div class="swiper-slide">
         <div
-          class="bg-center bg-no-repeat bg-cover py-44 lg:py-80"
+          class="py-10 bg-center bg-no-repeat bg-cover lg:py-52"
           style="background-image: url('/images/home/slider/1 - Tron 500 HD.jpg')"
         >
-          <div class="px-16">
-            <div class="lg:mx-10">
+          <div class="px-16 pt-20">
+            <div class="lg:ml-56">
               <p
-                class="text-xl text-center text-white lg:text-4xl 2xl:text-5xl"
+                class="w-5/6 text-3xl font-bold text-center text-white 2xl:w-3/6 lg:text-4xl 2xl:text-6xl"
               >
                 Detergentes Liquidos Biodegradables
               </p>
               <p
-                class="mt-32 text-xl text-center text-white lg:text-4xl 2xl:text-5xl"
+                class="w-5/6 text-2xl text-center text-white xl:pt-40 xl:mt-20 mt-60 md:4/6 2xl:w-4/6 lg:text-4xl 2xl:text-5xl"
               >
                 Completo lavado y desinfección de instalaciones incluyendo
                 equipos e implementos
@@ -26,18 +30,18 @@
 
       <div class="swiper-slide">
         <div
-          class="bg-center bg-no-repeat bg-cover py-44 lg:py-80"
+          class="py-48 bg-center bg-no-repeat bg-cover lg:py-80"
           style="background-image: url('/images/home/slider/2 - Tron 20 - 20.jpg')"
         >
           <div class="px-16">
             <div class="lg:mx-10">
               <p
-                class="text-xl text-center text-white lg:text-4xl 2xl:text-5xl"
+                class="w-4/6 text-2xl text-center text-white lg:text-4xl 2xl:text-5xl "
               >
                 Detergentes Liquidos Biodegradables
               </p>
               <p
-                class="mt-32 text-xl text-center text-white lg:text-4xl 2xl:text-5xl"
+                class="w-4/6 mt-32 text-2xl text-center text-white xl:w-3/6 lg:text-4xl 2xl:text-5xl"
               >
                 Completo lavado y desinfección de instalaciones incluyendo
                 equipos e implementos
@@ -48,39 +52,39 @@
       </div>
       <div class="swiper-slide">
         <div
-          class="bg-center bg-no-repeat bg-cover py-44 lg:py-56"
+          class="py-32 bg-center bg-no-repeat bg-cover lg:py-44"
           style="background-image: url('/images/home/slider/3 - Oxytron.jpg')"
         >
           <div class="px-16">
             <div class="lg:mx-10">
               <p
-                class="text-xl text-right text-white lg:text-4xl 2xl:text-5xl"
+                class="mt-24 text-2xl text-right text-white lg:text-4xl 2xl:text-5xl"
               >
                 Mezcla balanceada
               </p>
               <p
-                class="text-xl text-right text-white lg:text-4xl 2xl:text-5xl"
+                class="text-2xl text-right text-white lg:text-4xl 2xl:text-5xl"
               >
                 de detergente +
               </p>
               <p
-                class="text-xl text-right text-white lg:text-4xl 2xl:text-5xl"
+                class="text-2xl text-right text-white lg:text-4xl 2xl:text-5xl"
               >
                 desinfectante
               </p>
 
               <p
-                class="mt-4 text-xl text-right text-white lg:text-4xl 2xl:text-5xl"
+                class="mt-10 text-2xl text-right text-white lg:text-4xl 2xl:text-5xl"
               >
                 Ofrece todos los beneficios
               </p>
               <p
-                class="text-xl text-right text-white lg:text-4xl 2xl:text-5xl"
+                class="text-2xl text-right text-white lg:text-4xl 2xl:text-5xl"
               >
                 del oxigeno activo
               </p>
               <p
-                class="mt-40 text-xl text-center text-white lg:text-4xl 2xl:text-5xl"
+                class="text-3xl font-bold text-center text-white xl:mt-56 lg:text-4xl 2xl:text-5xl"
               >
                 Inocuidad alimentaria
               </p>
@@ -91,22 +95,17 @@
 
       <div class="swiper-slide">
         <div
-          class="bg-center bg-no-repeat bg-cover py-44 lg:py-80"
+          class="py-56 bg-center bg-no-repeat bg-cover lg:py-96"
           style="background-image: url('/images/home/slider/6 - Rax 6-86.jpg')"
         >
-          <div class="px-16">
+          <div class="px-16 py-20">
             <div class="lg:mx-10">
               <p
                 class="text-xl text-center text-white lg:text-4xl 2xl:text-5xl"
-              >
-                Detergentes Liquidos Biodegradables
-              </p>
+              ></p>
               <p
-                class="mt-32 text-xl text-center text-white lg:text-4xl 2xl:text-5xl"
-              >
-                Completo lavado y desinfección de instalaciones incluyendo
-                equipos e implementos
-              </p>
+                class="text-xl text-center text-white lg:text-4xl 2xl:text-5xl"
+              ></p>
             </div>
           </div>
         </div>
@@ -114,43 +113,41 @@
 
       <div class="swiper-slide">
         <div
-          class="bg-center bg-no-repeat bg-cover py-44 lg:py-80"
+          class="pt-64 pb-10 bg-center bg-no-repeat bg-cover lg:pt-80"
           style="background-image: url('/images/home/slider/4 -  Ferreterias.jpg')"
         >
-          <div class="px-16">
-            <div class="lg:mx-10">
+          <div class="px-16 -mt-36">
+            <div class="flex justify-center lg:mx-10">
               <p
-                class="text-xl text-center text-white lg:text-4xl 2xl:text-5xl"
+                class="mb-56 text-xl text-center text-white lg:text-4xl 2xl:text-5xl xl:w-4/6"
               >
-                Productos químico especializados para mentenimineto industrial 
-              </p>
-              <p
-                class="text-xl text-center text-white mt-44 lg:text-4xl 2xl:text-5xl"
-              >
-                Ferretería
+                Productos químico especializados para mentenimineto industrial
               </p>
             </div>
           </div>
+          <p
+            class="mt-40 text-xl text-center text-white lg:text-4xl xl:mt-96 2xl:text-5xl"
+          >
+            Ferretería
+          </p>
         </div>
       </div>
 
       <div class="swiper-slide">
         <div
-          class="bg-center bg-no-repeat bg-cover py-44 lg:py-80"
+          class="py-40 bg-center bg-no-repeat bg-cover lg:py-80"
           style="background-image: url('/images/home/slider/5 - Iron Cleaner.jpg')"
         >
           <div class="px-16">
             <div class="lg:mx-10">
               <p
-                class="mt-20 text-xl text-center text-white lg:text-4xl 2xl:text-5xl"
+                class="text-xl text-center text-white mt-28 lg:text-4xl 2xl:text-5xl"
               >
                 Desoxidantes de superficies
               </p>
               <p
                 class="text-xl text-center text-white mt-36 lg:text-4xl 2xl:text-5xl"
-              >
-                
-              </p>
+              ></p>
             </div>
           </div>
         </div>
@@ -218,4 +215,3 @@ export default {
   @apply w-48 h-48;
 } */
 </style>
-
